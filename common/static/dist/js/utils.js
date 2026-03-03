8161647f41c40fa531dfc561dec1a6aaeb56dc9e
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 19:23:46 +0100

    Translate Chinese UI strings and comments in utils.js

--- common/static/dist/js/utils.js
+++ common/static/dist/js/utils.js
@@ -1,22 +1,22 @@
 var onLoadErrorCallback = function (status, jqXHR) {
     if (status === 403) {
-        alert("权限错误，您没有权限查看该数据！");
+        alert("Permission error: you do not have permission to view this data!");
     } else if (jqXHR.statusText === "abort" || jqXHR.statusText === "canceled"){
         return 0
     } else {
-        alert("未知错误，请联系管理员处理！");
+        alert("Unknown error, please contact the administrator!");
     }
 };
 
 var dateFormat = function(fmt, date) {
     var o = {
-        "M+": date.getMonth() + 1,                   //月份
-        "d+": date.getDate(),                        //日
-        "h+": date.getHours(),                       //小时
-        "m+": date.getMinutes(),                     //分
-        "s+": date.getSeconds(),                     //秒
-        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
-        "S": date.getMilliseconds()                  //毫秒
+        "M+": date.getMonth() + 1,                    // month
+        "d+": date.getDate(),                         // day
+        "h+": date.getHours(),                        // hour
+        "m+": date.getMinutes(),                      // minute
+        "s+": date.getSeconds(),                      // second
+        "q+": Math.floor((date.getMonth() + 3) / 3), // quarter
+        "S": date.getMilliseconds()                   // millisecond
     };
     if(/(y+)/.test(fmt))
         fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
@@ -26,7 +26,7 @@ var dateFormat = function(fmt, date) {
     return fmt;
 };
 
-// 格式与高亮json格式的字符串
+// Format and highlight JSON strings
 var jsonHighLight = function(json) {
     json = json.toString().replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
@@ -47,7 +47,7 @@ var jsonHighLight = function(json) {
     });
 };
 
-// 实例配置页面根据db_type选择显示或隐藏mode字段，mode字段只适用于redis实例
+// On the instance config page, show/hide the mode field by db_type (mode only applies to redis)
 (function($) {
     $(function() {
         let db_type = $('#id_db_type');
@@ -65,23 +65,23 @@ var jsonHighLight = function(json) {
     });
 })(jQuery);
 
-// 短信验证码倒计时
+// SMS verification code countdown
 let countdown = 60, clock, btn_captcha;
 function init_countdown(obj) {
     btn_captcha = obj;
     btn_captcha.disabled = true;
-    btn_captcha.innerText = countdown + ' 秒后重试';
+    btn_captcha.innerText = countdown + 's to retry';
     clock = setInterval(countdown_loop, 1000);
 }
 
 function countdown_loop() {
     countdown--;
     if(countdown > 0){
-        btn_captcha.innerText = countdown + ' 秒后重试';
+        btn_captcha.innerText = countdown + 's to retry';
     } else {
         clearInterval(clock);
         btn_captcha.disabled = false;
-        btn_captcha.innerText = '获取验证码';
+        btn_captcha.innerText = 'Get verification code';
         countdown = 60;
     }
 }
