d3e06f77b01ec7b845412552a0eea37ef06e5b4c
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 20:08:05 +0100

    Translate Chinese text in v1.3.0.sql

--- src/init_sql/v1.3.0.sql
+++ src/init_sql/v1.3.0.sql
@@ -1,19 +1,19 @@
--- 增加工单日志表
+-- Add workflow log table
 CREATE TABLE `workflow_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `audit_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '工单审批id',
-  `operation_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '操作类型，0提交/待审核、1审核通过、2审核不通过、3审核取消/取消执行、4定时执行、5执行工单、6执行结束',
-  `operation_type_desc` char(10) NOT NULL DEFAULT '' COMMENT '操作类型描述',
-  `operation_info` varchar(200) NOT NULL DEFAULT '' COMMENT '操作信息',
-  `operator` varchar(30) NOT NULL DEFAULT '' COMMENT '操作人',
-  `operator_display` varchar(50) NOT NULL DEFAULT '' COMMENT '操作人中文名',
-  `operation_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '操作时间',
+  `audit_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'Workflow audit ID',
+  `operation_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT 'Operation type: 0 submit/pending review, 1 approved, 2 rejected, 3 canceled/cancel execution, 4 scheduled execution, 5 execute workflow, 6 execution finished',
+  `operation_type_desc` char(10) NOT NULL DEFAULT '' COMMENT 'Operation type description',
+  `operation_info` varchar(200) NOT NULL DEFAULT '' COMMENT 'Operation info',
+  `operator` varchar(30) NOT NULL DEFAULT '' COMMENT 'Operator',
+  `operator_display` varchar(50) NOT NULL DEFAULT '' COMMENT 'Operator display name',
+  `operation_time` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT 'Operation time',
   PRIMARY KEY (`id`),
   index idx_audit_id(audit_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- 增加菜单权限
+-- Add menu permissions
 set @content_type_id=(select id from django_content_type where app_label='sql' and model='permission');
-INSERT INTO auth_permission (name, content_type_id, codename) VALUES ('菜单 数据库审核', @content_type_id, 'menu_themis');
-INSERT INTO auth_permission (name, content_type_id, codename) VALUES ('菜单 实例管理', @content_type_id, 'menu_instance');
-INSERT INTO auth_permission (name, content_type_id, codename) VALUES ('菜单 Binlog2SQL', @content_type_id, 'menu_binlog2sql');
+INSERT INTO auth_permission (name, content_type_id, codename) VALUES ('Menu Database Review', @content_type_id, 'menu_themis');
+INSERT INTO auth_permission (name, content_type_id, codename) VALUES ('Menu Instance Management', @content_type_id, 'menu_instance');
+INSERT INTO auth_permission (name, content_type_id, codename) VALUES ('Menu Binlog2SQL', @content_type_id, 'menu_binlog2sql');
