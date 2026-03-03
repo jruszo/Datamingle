14f7eabd643c259a4538e73a4e60dae13cdcb9ce
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 20:04:31 +0100

    Translate Chinese text in v1.4.0.sql

--- src/init_sql/v1.4.0.sql
+++ src/init_sql/v1.4.0.sql
@@ -1,12 +1,12 @@
--- 新增登录失败信息
+-- Add login failure information
 alter table sql_users
-  add failed_login_count tinyint not null default 0 comment '失败计数',
-  add last_login_failed_at timestamp comment '上次失败登录时间';
+  add failed_login_count tinyint not null default 0 comment 'Failure count',
+  add last_login_failed_at timestamp comment 'Last failed login time';
 
--- 修改资源表名
+-- Rename resource tables
 rename table sql_group to resource_group;
 rename table sql_group_relations to resource_group_relations;
 
--- 使用django_q替换django_apscheduler
+-- Replace django_apscheduler with django_q
 drop table django_apscheduler_djangojobexecution;
 drop table django_apscheduler_djangojob;
