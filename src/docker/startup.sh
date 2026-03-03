0818e2b96c4604d34c0ecc67598c2baeccba30f4
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 18:19:58 +0100

    Translate src/docker/startup.sh messages to English

--- src/docker/startup.sh
+++ src/docker/startup.sh
@@ -2,29 +2,28 @@
 
 cd /opt/archery
 
-echo 切换python运行环境
+echo Switch Python runtime environment
 source /opt/venv4archery/bin/activate
 #pip install -r requirements.txt -i https://mirrors.ustc.edu.cn/pypi/web/simple/
 
-echo 修改重定向端口
+echo Update redirect port
 if [[ -z $NGINX_PORT ]]; then
     sed -i "s/:nginx_port//g" /etc/nginx/nginx.conf
 else
     sed -i "s/nginx_port/$NGINX_PORT/g" /etc/nginx/nginx.conf
 fi
 
-echo 启动nginx
+echo Start nginx
 /usr/sbin/nginx
 
-echo 收集所有的静态文件到STATIC_ROOT
+echo Collect all static files into STATIC_ROOT
 python3 manage.py collectstatic -v0 --noinput
 
-echo 启动Django Q cluster
+echo Start Django Q cluster
 supervisord -c /etc/supervisord.conf
 
-echo 启动服务
+echo Start services
 gunicorn -w 4 -b 127.0.0.1:8888 --timeout 600 archery.wsgi:application
-
 
 
 
