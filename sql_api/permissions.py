7b1ed3a3ab93b604126f51e9597fa69ddc290437
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 17:48:21 +0100

    Translate sql_api/permissions.py to English

--- sql_api/permissions.py
+++ sql_api/permissions.py
@@ -4,7 +4,7 @@ from common.config import SysConfig
 
 class IsInUserWhitelist(permissions.BasePermission):
     """
-    自定义权限，只允许白名单用户调用api
+    Custom permission that only allows whitelisted users to call the API.
     """
 
     def has_permission(self, request, view):
@@ -12,13 +12,13 @@ class IsInUserWhitelist(permissions.BasePermission):
         user_list = config.split(",") if config else []
         api_user_whitelist = [int(uid) for uid in user_list]
 
-        # 只有在api_user_whitelist参数中的用户才有权限
+        # Only users listed in api_user_whitelist are allowed.
         return request.user.id in api_user_whitelist
 
 
 class IsOwner(permissions.BasePermission):
     """
-    当参数engineer与请求用户一致时才有权限
+    Permission is granted only when the `engineer` parameter matches the request user.
     """
 
     def has_permission(self, request, view):
