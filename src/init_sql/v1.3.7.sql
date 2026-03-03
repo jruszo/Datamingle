f98288c7cd71479e0d08c0d0c3d6fdb076d82a8a
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 19:30:37 +0100

    Translate Chinese text in v1.3.7.sql

--- src/init_sql/v1.3.7.sql
+++ src/init_sql/v1.3.7.sql
@@ -1,2 +1,2 @@
--- 修改阿里云配置信息表
-alter table aliyun_rds_config add is_enable tinyint not null default 0 comment '是否启用';
+-- Update Alibaba Cloud configuration table
+alter table aliyun_rds_config add is_enable tinyint not null default 0 comment 'Enabled or not';
