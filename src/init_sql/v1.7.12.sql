491850b1f70ec68faa1ed6a77827efd45b2ff4b3
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 19:34:24 +0100

    Translate Chinese text in v1.7.12.sql

--- src/init_sql/v1.7.12.sql
+++ src/init_sql/v1.7.12.sql
@@ -1,4 +1,4 @@
--- 增加ssh_tunnel配置表
+-- Add ssh_tunnel configuration table
 CREATE TABLE `ssh_tunnel` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `tunnel_name` varchar(50) NOT NULL,
@@ -18,5 +18,5 @@ alter table sql_instance add `tunnel_id` int(11) DEFAULT NULL,
 add index `idx_ssh_tunnel_id` (`tunnel_id`),
 add CONSTRAINT `sql_instance_tunnel_id_99377638_fk_ssh_tunnel_id` FOREIGN KEY (`tunnel_id`) REFERENCES `ssh_tunnel` (`id`);
 
--- 增加企业微信信息
-alter table resource_group add qywx_webhook varchar(255) not null default '' comment '企业微信webhook地址';
+-- Add WeCom configuration
+alter table resource_group add qywx_webhook varchar(255) not null default '' comment 'WeCom webhook URL';
