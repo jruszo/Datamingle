fe1f4dfab9149c0ce43535b331106d1aa5a7d24b
Janos Ruszo <janos.ruszo@real-digital.de>
2026-03-03 18:21:14 +0100

    Translate masking.sh comments to English

--- masking.sh
+++ masking.sh
@@ -1,8 +1,8 @@
-# 脱敏字段添加过于繁琐
-# 可参考下面的脚本，定时运行即可
+# Adding masking fields manually is cumbersome.
+# You can use the script below and run it on a schedule.
 
-# 脱敏规则
-# (1, '手机号'), (2, '证件号码'), (3, '银行卡'), (4, '邮箱'), (5, '金额'), (6, '其他')
+# Masking rules
+# (1, 'Phone Number'), (2, 'ID Number'), (3, 'Bank Card'), (4, 'Email'), (5, 'Amount'), (6, 'Other')
 masking_rule_phone='phone|mobile'
 masking_rule_idno='id_number|idcard'
 masking_rule_bankcardno='bank_no'
@@ -19,20 +19,20 @@ archery_user=
 archery_db=archery
 archery_pw=
 
-# 获取archery所有slave实例信息
+# Get all Archery slave instance info
 mysql -h$archery_host -P$archery_port -u$archery_user -p$archery_pw $archery_db -N -e "select 
 id,instance_name,host,port 
 from sql_instance  where type='slave';">instances.list
 
-# 清空表
+# Truncate table
 mysql -h$archery_host -P$archery_port -u$archery_user -p$archery_pw $archery_db -N -e "truncate table data_masking_columns;"
 
-# 临时账号密码（因实例账号&密码为加密，写死使用）
-# 此方式只适用单个实例或多个实例账号密码一致
+# Temporary account/password (instance credentials are encrypted, so use fixed values here)
+# This method only works for a single instance or multiple instances sharing the same credentials
 user=
 pw=
 
-# 获取脱敏字段信息
+# Get masking field information
 cat instances.list|while read instance_name host port 
 do 
 mysql -h$host -P$port -u$user -p$pw -N -e "
@@ -61,6 +61,6 @@ WHERE COLUMN_NAME REGEXP '$masking_rules'
 AND TABLE_SCHEMA != 'performance_schema'
 AND TABLE_SCHEMA != 'information_schema';">$instance_name.txt
 
-# 更新表数据
+# Update table data
 mysql -h$archery_host -P$archery_port -u$archery_user -p$archery_pw $archery_db -N -e "load data local infile '$instance_name.txt' replace into table data_masking_columns fields terminated by '\t' ( rule_type,active,instance_id,table_schema,table_name,column_name,column_comment);"
 done
